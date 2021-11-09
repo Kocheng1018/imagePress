0ef8b4270dc964918e11e640747c7028ec5c156e
docker-TYR <docker210106@tyr-tech.com>
2021-11-09 11:32:47 +0800

    refactor: build pool

--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	github.com/h2non/bimg v1.1.5
 )
 
-require github.com/zenthangplus/goccm v0.0.0-20211005163543-2f2e522aca15 // indirect
+require (
+	github.com/panjf2000/ants/v2 v2.4.6 // indirect
+	github.com/zenthangplus/goccm v0.0.0-20211005163543-2f2e522aca15 // indirect
+)
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/h2non/bimg"
+	"github.com/panjf2000/ants/v2"
 )
 
 func main() {
@@ -24,8 +25,15 @@ func main() {
 	spew.Dump("所需時間:", timeBlock)
 }
 
+type TmpStruct struct {
+	buffer      []byte
+	options     bimg.Options
+	newFileName string
+}
+
 func listfile(path string) {
 	wg := new(sync.WaitGroup)
+
 	files, _ := ioutil.ReadDir("in")
 	options := bimg.Options{
 		Quality: 60,
@@ -34,38 +42,48 @@ func listfile(path string) {
 		// Speed:       8,
 	}
 
-	wg.Add(len(files)-1)
-	limitCh := make(chan struct{}, 15)
+	p, _ := ants.NewPoolWithFunc(10, func(in interface{}) {
+		st := in.(TmpStruct)
+		imagePress(st.buffer, st.options, st.newFileName)
+		wg.Done()
+	})
 
+	defer p.Release()
+
+	wg.Add(len(files))
 	for _, file := range files {
-		if file.Name() == "README.md" {
-			continue
+		// if file.Name() == "README.md" {
+		// 	continue
+		// }
+		// if file.IsDir() {
+		// 	listfile(path + "/in/" + file.Name())
+		// 	continue
+		// }
+		spew.Dump(fmt.Sprintf("run:%s", file.Name()))
+
+		buffer, err := bimg.Read("./in/" + file.Name())
+		if err != nil {
+			spew.Dump(os.Stderr, err)
 		}
-		if file.IsDir() {
-			listfile(path + "/in/" + file.Name())
-		} else {
-			limitCh <- struct{}{}
-			spew.Dump(fmt.Sprintf("run:%s", file.Name()))
 
-			buffer, err := bimg.Read("./in/" + file.Name())
-			if err != nil {
-				spew.Dump(os.Stderr, err)
-			}
+		newfileName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
 
-			newfileName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
+		p.Invoke(TmpStruct{
+			buffer:      buffer,
+			newFileName: newfileName,
+			options:     options,
+		})
 
-			go imagePress(buffer, options, newfileName, wg, limitCh)
-		}
+		// syncCalculateSum := func() {
+		// 	imagePress(buffer, options, newfileName)
+		// 	wg.Done()
+		// }
+		// _ = ants.Submit(syncCalculateSum)
 	}
 	wg.Wait()
 }
 
-func imagePress(buffer []byte, options bimg.Options, newFileName string, wg *sync.WaitGroup, limitCh chan struct{}) {
-	defer func() {
-		<-limitCh
-		wg.Done()
-	}()
-
+func imagePress(buffer []byte, options bimg.Options, newFileName string) {
 	if bimg.NewImage(buffer).Type() != "jpeg" &&
 		bimg.NewImage(buffer).Type() != "heif" &&
 		bimg.NewImage(buffer).Type() != "webp" &&
